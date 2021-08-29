2b674d2abe89a5725dd9539c7152e215fd274498
Tommasory <tommasory@gmail.com>
2021-08-29 16:10:28 -0500

    Agrega conexion entre tenplates por medio de las urls de las aplicaciones.

---
+++ blog/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .views import BlogListView
+app_name = "blog"
+
+urlpatterns = [
+    path('', BlogListView.as_view(), name='home'),
+]
--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,11 @@
+from django.conf.urls import include
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from .views import HomeView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',HomeView.as_view(), name='Home'),
+    path('blog/',include('blog.urls', namespace='blog')),
 ]
