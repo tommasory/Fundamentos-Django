16f09511c5de5cbac510c1b0decdca9c0ac36496
Tommasory <tommasory@gmail.com>
2021-08-29 13:40:36 -0500

    Agrega configuración basica de una vista.

---
+++ core/views.py
@@ -0,0 +1,10 @@
+from django.views.generic import View
+from django.shortcuts import render
+
+class Home(View):
+
+    def get(self, request, *args, **kwargs):
+        context = {
+
+        }
+        return render(request, '', context)
