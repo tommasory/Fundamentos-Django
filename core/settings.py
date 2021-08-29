fc421c1b7162719c675fabb4a9ecf37269782ec4
Tommasory <tommasory@gmail.com>
2021-08-29 14:16:16 -0500

    Agrega las configuraciones de Django a variables de ambiente.

--- core/settings.py
+++ core/settings.py
@@ -1,6 +1,10 @@
 from pathlib import Path
-
 import os
+# Para esto necesitamos instalar la libreria <django-environ>
+import environ
+
+env = environ.Env()
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -10,10 +14,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-6u%iz+g)$)+*hj)f6%y)kfbhxfmy3jaxpx_2$cwqi=b37bx1#d'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG')
 
 ALLOWED_HOSTS = []
 
