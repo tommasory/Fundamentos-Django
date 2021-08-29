69d521808f02eab55f5cf6bf0206ec1b07651460
Tommasory <tommasory@gmail.com>
2021-08-29 14:33:18 -0500

    Agrega app para el blog.

--- core/settings.py
+++ core/settings.py
@@ -24,7 +24,7 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
-INSTALLED_APPS = [
+BASE_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -32,6 +32,15 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 ]
+
+LOCAL_APPS = [
+    'blog',
+]
+
+THIRD_APPS = [
+]
+
+INSTALLED_APPS = BASE_APPS + LOCAL_APPS + THIRD_APPS
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
