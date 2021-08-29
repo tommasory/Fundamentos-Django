46beaf32305687c8830b6a958c9b6e35d24e8068
Tommasory <tommasory@gmail.com>
2021-08-29 16:33:18 -0500

    Agrega la creación de un forms.

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import BlogListView
+from .views import BlogListView, BlogCreateView
 app_name = "blog"
 
 urlpatterns = [
     path('', BlogListView.as_view(), name='home'),
+    path('create/', BlogCreateView.as_view(), name='create'),
 ]
