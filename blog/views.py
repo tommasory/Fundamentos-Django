c5291ad505ecd7e7443520ecbdef1b95e7e08da5
Tommasory <tommasory@gmail.com>
2021-08-29 19:44:45 -0500

    Agrega la opción de mirar los detalles de un post.

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import get_object_or_404, render, redirect
 from django.views.generic import View
 from .forms import PostCreateForm
 from .models import Post
@@ -6,8 +6,9 @@ from .models import Post
 class BlogListView(View):
 
     def get(self, request, *args, **kwargs):
+        posts = Post.objects.all()
         context = {
-
+            'posts':posts
         }
         return render(request, 'blog_list.html', context)
 
@@ -34,3 +35,11 @@ class BlogCreateView(View):
 
         }
         return render(request, 'blog_create.html', context)
+
+class BlogDetailView(View):
+    def get(self, request, pk, *args, **kwargs):
+        post = get_object_or_404(Post, pk=pk)
+        context = {
+            'post':post
+        }
+        return render(request, 'blog_detail.html', context)
